62289884021b24c6a32f54f59e4ce6c5cb85847d
zenialexandre <zenialexandre123@gmail.com>
2024-04-03 23:22:03 -0300

    refactor/feat: refactoring general code / adding dataset scatter plots

--- demo.py
+++ demo.py
@@ -1,1 +1,48 @@
-#....
+import numpy as np
+import matplotlib.pyplot as plt
+from regression_utils import correlation_coefficient, linear_regression
+
+first_dataset_matrix: list = np.array(
+    [
+        [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+        [8.04, 6.95, 7.58, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68]
+    ]
+)
+second_dataset_matrix: list = np.array(
+    [
+        [10, 8, 13, 9, 11, 14, 6, 4, 12, 7, 5],
+        [9.14, 8.14, 8.47, 8.77, 9.26, 8.10, 6.13, 3.10, 9.13, 7.26, 4.74]
+    ]
+)
+third_dataset_matrix: list = np.array(
+    [
+        [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 19],
+        [6.58, 5.76, 7.71, 8.84, 8.47, 7.04, 5.25, 5.56, 7.91, 6.89, 12.50]
+    ]
+)
+
+def create_dataset_matrix_scatter_plots(
+    first_dataset_matrix: list,
+    second_dataset_matrix: list,
+    third_dataset_matrix: list
+) -> None:
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+    fig.canvas.manager.set_window_title('Scatter Plots of the Datasets')
+
+    ax[0].scatter(x=first_dataset_matrix[0], y=first_dataset_matrix[1], color='red')
+    ax[0].set_title('Scatter Plot of the First Dataset')
+    
+    ax[1].scatter(x=second_dataset_matrix[0], y=second_dataset_matrix[1], color='blue', label='Second Dataset')
+    ax[1].set_title('Scatter Plot of the Second Dataset')
+    
+    ax[2].scatter(x=third_dataset_matrix[0], y=third_dataset_matrix[1], color='green', label='Third Dataset')
+    ax[2].set_title('Scatter Plot of the Third Dataset')
+
+    plt.tight_layout()
+    plt.show()
+
+create_dataset_matrix_scatter_plots(
+    first_dataset_matrix,
+    second_dataset_matrix,
+    third_dataset_matrix
+)
--- regression_utils.py
+++ regression_utils.py
@@ -1,49 +1,60 @@
 import numpy as np
 import math
 
-def corelation_coefficient(array):
-    len_array = len(array[0])
-    x_mean = np.mean(array[0])
-    y_mean = np.mean(array[1])
-    sum_numerator = 0
-    sum_denominator_x = 0
-    sum_denominator_y = 0
+def correlation_coefficient(dataset_matrix: list) -> float:
+    (dataset_matrix_x_mean, dataset_matrix_y_mean) = get_dataset_matrix_x_and_y_means(dataset_matrix) 
+    correlation_coefficient_numerator: float = 0.0
+    correlation_coefficient_denominator_x: float = 0.0
+    correlation_coefficient_denominator_y: float = 0.0
 
-    for idx in range(len_array):
-        sum_numerator += (array[0][idx] - x_mean)*(array[1][idx] - y_mean)
-        sum_denominator_x += (array[0][idx] - x_mean)**2
-        sum_denominator_y += (array[1][idx] - y_mean)**2
+    for index in range(len(dataset_matrix[0])):
+        correlation_coefficient_numerator += \
+            (dataset_matrix[0][index] - dataset_matrix_x_mean) * (dataset_matrix[1][index] - dataset_matrix_y_mean)
+        correlation_coefficient_denominator_x += (dataset_matrix[0][index] - dataset_matrix_x_mean) ** 2
+        correlation_coefficient_denominator_y += (dataset_matrix[1][index] - dataset_matrix_y_mean) ** 2
 
-    return sum_numerator / math.sqrt(sum_denominator_x * sum_denominator_y)
+    return correlation_coefficient_numerator / math.sqrt(
+        correlation_coefficient_denominator_x * correlation_coefficient_denominator_y
+    )
 
+def linear_regression(dataset_matrix: list) -> list:
+    (dataset_matrix_x_mean, dataset_matrix_y_mean) = get_dataset_matrix_x_and_y_means(dataset_matrix)
+    beta_1: float = get_calculated_beta_1(
+        dataset_matrix,
+        dataset_matrix_x_mean,
+        dataset_matrix_y_mean
+    )
+    beta_0 = get_calculated_beta_0(
+        beta_1,
+        dataset_matrix_x_mean,
+        dataset_matrix_y_mean
+    )
+    y_hat: list = []
 
-def linear_regression(array):
-
-    x_mean = np.mean(array[0])
-    y_mean = np.mean(array[1])
-
-    beta_1 = caculate_beta_1(array, x_mean, y_mean)
-    beta_0 = calculate_beta_0(beta_1, x_mean, y_mean)
-
-    y_hat = []
-    for x in array[0]:
-        y_hat.append(beta_0 + beta_1*x)
-        
+    for cell_x in dataset_matrix[0]: y_hat.append(beta_0 + beta_1 * cell_x)
     return y_hat
 
-def caculate_beta_1(array, x_mean, y_mean):
+def get_dataset_matrix_x_and_y_means(dataset_matrix: list) -> tuple[float, float]:
+    return (np.mean(dataset_matrix[0]), np.mean(dataset_matrix[1]))
 
-    len_array = len(array[0])
+def get_calculated_beta_1(
+    dataset_matrix: list,
+    dataset_matrix_x_mean: float,
+    dataset_matrix_y_mean: float
+) -> float:
+    calculus_numerator: float = 0.0
+    calculus_denominator: float = 0.0
 
-    sum_numerator = 0
-    sum_dominator = 0
+    for index in range(len(dataset_matrix[0])):
+        calculus_numerator += \
+            (dataset_matrix[0][index] - dataset_matrix_x_mean) * (dataset_matrix[1][index] - dataset_matrix_y_mean)
+        calculus_denominator += (dataset_matrix[0][index] - dataset_matrix_x_mean) ** 2
 
-    for idx in range(len_array):
-        sum_numerator += (array[0][idx] - x_mean)*(array[1][idx] - y_mean)
-        sum_dominator += (array[0][idx] - x_mean)**2
-    
-    
-    return round(sum_numerator / sum_dominator,4)
+    return round(calculus_numerator / calculus_denominator, 4)
 
-def calculate_beta_0(beta_1, x_mean, y_mean):
-    return round(y_mean - beta_1*x_mean,4)
+def get_calculated_beta_0(
+    beta_1: float,
+    dataset_matrix_x_mean: float,
+    dataset_matrix_y_mean: float
+) -> float:
+    return round(dataset_matrix_y_mean - beta_1 * dataset_matrix_x_mean, 4)
